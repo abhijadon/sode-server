8f012d3cf1ac1d566907c95d4610792d8dffd059
Abhishek jadon <abhishekjadon@192.168.1.68>
2026-07-31 13:10:16 +0530

    adf

--- test_api3.js
+++
@@ -1,7 +0,0 @@
-const http = require("http");
-const req = http.get("http://127.0.0.1:5001/api/website/courses/website-list?category=certification&subcategory=ai-courses", (res) => {
-  let data = "";
-  res.on("data", chunk => data += chunk);
-  res.on("end", () => console.log("Response JSON length:", data.length, "Status:", res.statusCode));
-});
-req.on("error", err => console.error("Error:", err.message));
